c23c3cab741b79e3b0e070aa66ea3253148aca99
Yauheni_Vakulchyk <yauheni_vakulchyk@epam.com>
2018-02-07 00:10:18 +0300

    Implemented task 7. Part 3

--- src/app/courses-page/courses-page.component.ts
+++ src/app/courses-page/courses-page.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CourseComponent} from './course/course.component';
 import {CourseService} from '../services/course.service';
-import {ModalService} from '../services/modal.service';
 import {FindPipe} from '../pipes/find.pipe';
 
 @Component({
@@ -14,23 +13,20 @@ export class CoursesPageComponent implements OnInit {
   courses: CourseComponent[];
   isListEmpty: boolean = false;
 
-  constructor(private courseService: CourseService, private modalService: ModalService, private findPipe: FindPipe) {
+  currentPage: number;
+  pageItems: number[];
+
+  constructor(private courseService: CourseService, private findPipe: FindPipe) {
   }
 
   ngOnInit() {
     console.log('CoursesPageComponent init');
    // this.courses = this.courseService.getList();
-
-    this.courseService.getList().subscribe((courses: CourseComponent[]) => {
-      this.courses = courses;
-      console.log(courses);
-    });
+    this.currentPage = 1;
+    this.getList();
 
     this.courseService.coursesData.subscribe(data => {
-      this.courseService.getList().subscribe((courses: CourseComponent[]) => {
-        this.courses = courses;
-        this.checkList(this.courses);
-      });
+      this.getList();
     });
 
     /*this.courseService.searchText.subscribe((searchText) => {
@@ -40,11 +36,50 @@ export class CoursesPageComponent implements OnInit {
     });*/
   }
 
+  getList() {
+    this.courseService.getList().subscribe((courses: CourseComponent[]) => {
+      // this.courses = courses;
+      console.log(courses);
+      this.setCourses((this.currentPage - 1) * 10, courses);
+      this.checkList(this.courses);
+      this.pageItems = this.range(1, Math.ceil(courses.length / 10) + 1 , 1);
+    });
+  }
+
+  setCourses(startIndex: number, courses: CourseComponent[]) {
+    this.courses = [];
+    let to: number = startIndex + 10;
+
+    if (courses.length < to) {
+      to = courses.length;
+    }
+    for (let i = startIndex; i < to; i++) {
+      this.courses.push(courses[i]);
+    }
+  }
+
   checkList(courses: CourseComponent[]) {
     if (courses.length === 0) {
       this.isListEmpty = true;
     } else {
       this.isListEmpty = false;
     }
+  }
+
+  range(start, edge, step): any[] {
+    if (arguments.length === 1) {
+      edge = start;
+      start = 0;
+    }
+
+
+    edge = edge || 0;
+    step = step || 1;
+
+    let ret;
+    for (ret = []; (edge - start) * step > 0; start += step) {
+      ret.push(start);
+    }
+    return ret;
   }
 }
