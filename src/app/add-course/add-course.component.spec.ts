6c08f0618c7293cf89db37578a01b3fd3b9b5265
Yauheni_Vakulchyk <yauheni_vakulchyk@epam.com>
2018-03-13 12:07:15 +0300

    Implemented task 11 part 2. Add tests

--- src/app/add-course/add-course.component.spec.ts
+++ src/app/add-course/add-course.component.spec.ts
@@ -27,9 +27,36 @@ describe('AddCourseComponent', () => {
     expect(component.myForm.valid).toBeFalsy();
   });
 
+  it('name invalid when empty', () => {
+    const name = component.myForm.controls['name'];
+    expect(name.valid).toBeFalsy();
+  });
+
   it('description invalid when empty', () => {
     const description = component.myForm.controls['description'];
     expect(description.valid).toBeFalsy();
+  });
+
+  it('duration invalid when empty', () => {
+    const duration = component.myForm.controls['duration'];
+    expect(duration.valid).toBeFalsy();
+  });
+
+  it('date invalid when empty', () => {
+    const date = component.myForm.controls['date'];
+    expect(date.valid).toBeFalsy();
+  });
+
+  it('name invalid when more than max text', () => {
+    const name = component.myForm.controls['name'];
+    name.setValue('qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm');
+    expect(name.valid).toBeFalsy();
+  });
+
+  it('name validation', () => {
+    const name = component.myForm.controls['name'];
+    name.setValue('test');
+    expect(name.valid).toBeTruthy();
   });
 
   it('description validation', () => {
